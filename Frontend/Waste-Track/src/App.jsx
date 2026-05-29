2038b5afb4bcde872a2b9dbfb2c8325861ee066b
Einon Cris O.Alcantara <aeinoncris@gmail.com>
2026-05-29 13:18:16 +0800

    Update and fix pwa setup

--- Frontend/Waste-Track/src/App.jsx
+++ Frontend/Waste-Track/src/App.jsx
@@ -2,14 +2,12 @@ import HomepageRoutes from "./routes/HomepageRoutes/HomepageRoutes";
 import AuthRoutes from "./routes/AuthRoutes/AuthRoutes";
 import UserRoutes from "./routes/UserRoutes/UserRoutes";
 import AdminRoutes from "./routes/AdminRoutes/AdminRoutes";
-import InstallButton from "./components/InstallButton";
 
 function App() {
   
 
   return (
     <>
-      <InstallButton />
       <HomepageRoutes />
       <AuthRoutes />
       <UserRoutes />
